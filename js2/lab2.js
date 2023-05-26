b4c605171b16b586b67abfcdc9ffb75cc053edbe
Sergey Yushkov <69847651+yushkear@users.noreply.github.com>
2023-05-26 12:53:46 +0300

    123

--- js2/lab2.js
+++ js2/lab2.js
@@ -1,12 +1,56 @@
-function pow(x, n) {
-    if (n < 0) return NaN;
-    if (Math.round(n) != n) return NaN;
-  
-    let result = 1;
-  
-    for (let i = 0; i < n; i++) {
-      result *= x;
+function pow(x, n)
+{
+    return x**n;
+}
+function sumTo(n)
+
+{
+    return (n*(n+1))/2
+
+}
+function factorial(n)
+{
+    if (n<=0)
+    {
+        return 1n;
     }
-  
+    else{
+        return ( BigInt (n) *  BigInt(factorial(n-1)));
+    }
+}
+function fib(n){
+    a = BigInt(1);
+    b=BigInt(1);
+    if (n== BigInt(0))
+    {
+        return BigInt(0);
+    }
+    else
+    {
+        for (i = BigInt(3); i<=n; i++)
+        {
+            c = a+b;
+            a=b;
+            b=c;
+        }
+        return b;
+    }
+}
+function compare(x)
+{
+return function(y)
+{
+    if (y>x) return true;
+    else if (y<x) return false;
+    else if (y==x) return null;
+}
+}
+function sum()
+{
+    var result=0;
+    for (var i=0; i<arguments.length; i++)
+    {
+        result+=arguments[i]
+    }
     return result;
-  }
+}
