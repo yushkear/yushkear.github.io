f9667bd3a3f8f1e0003585a4b8e7c3a8a2373dc4
Sergey Yushkov <69847651+yushkear@users.noreply.github.com>
2023-05-26 12:43:41 +0300

    123

--- js2/lab2.js
+++ js2/lab2.js
@@ -1,3 +1,9 @@
-function pow(x,y){
-    return 4; //проверка спецификации
-}
+function pow(x, n) {
+    let result = 1;
+  
+    for (let i = 0; i < n; i++) {
+      result *= x;
+    }
+  
+    return result;
+  }
