4c88947790f2a59e6c97723db20870c08639b0d2
Sergey Yushkov <69847651+yushkear@users.noreply.github.com>
2023-05-26 13:08:41 +0300

    123

--- js2/lab2.js
+++ js2/lab2.js
@@ -1,23 +1,27 @@
-function pow(x, n)
-{
-    return x**n;
-}
-function sumTo(n)
 
-{
-    return (n*(n+1))/2
+//функция возведения числа x в степень n
+function pow(x, n){
+    return x**n; //возвращается x в степени n
+} 
+
+//функция суммы чисел от 1 до n
+function sumTo(n){
+    return (n*(n+1))/2 //формула суммы чисел
 
 }
-function factorial(n)
-{
+
+//функция факториала числа n
+function factorial(n){
     if (n<=0)
     {
         return 1n;
     }
     else{
-        return ( BigInt (n) *  BigInt(factorial(n-1)));
+        return ( BigInt (n) *  BigInt(factorial(n-1))); //возвращает произведение числа n и факториала n-1
     }
 }
+
+//n-ое число фибоначи
 function fib(n){
     a = BigInt(1);
     b=BigInt(1);
@@ -29,26 +33,26 @@ function fib(n){
     {
         for (i = BigInt(3); i<=n; i++)
         {
-            c = a+b;
+            c = a+b; //цикл для числа как суммы двух предыдущих его чисел
             a=b;
             b=c;
         }
         return b;
     }
 }
-function compare(x)
-{
-return function(y)
-{
+
+//сравнение числа x и числа y из анонимной функции
+function compare(x){
+return function(y){ //сама анониная функция
     if (y>x) return true;
     else if (y<x) return false;
     else if (y==x) return null;
 }
 }
-function sum()
-{
-    var result=0;
-    for (var i=0; i<arguments.length; i++)
+//функция суммы всех своих аргументов
+function sum(){
+    var result = 0;
+    for (var i = 0; i < arguments.length; i++)
     {
         result+=arguments[i]
     }
