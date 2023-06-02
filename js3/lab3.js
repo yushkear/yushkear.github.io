f266aea71648f0cd471073747b4397c3c692868d
Sergey Yushkov <69847651+yushkear@users.noreply.github.com>
2023-06-02 12:01:10 +0300

    123

--- js3/lab3.js
+++ js3/lab3.js
@@ -1,6 +1,10 @@
 function getDecimal(num){
     switch (num){
+        case(num>=0):
+        return num - math.floor(num); //Если число меньше нуля - вычитай потолок,
+        break;                        //Если больше - вычитай пол.
         case(num<0):
-        return num
+        return num - math.ceil(num);
+        break;
     }
 }
