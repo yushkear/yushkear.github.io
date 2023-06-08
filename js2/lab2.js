4c6d1cd143878224b8d73b0616b69a2cc7e784e1
Sergey Yushkov <69847651+yushkear@users.noreply.github.com>
2023-06-08 16:06:44 +0300

    123

--- js2/lab2.js
+++ js2/lab2.js
@@ -1,60 +1,98 @@
 "use strict";
+
+/**
+ * Возвращает a, возведённое в b-ную степень.
+ *
+ * @param {number} a Возводимое в степень число.
+ * @param {number} b Степень, должна быть натуральным числом.
+ * @return {number} a ** b, возведённое в n-ную степень.
+ */
+
 //функция возведения числа x в степень n
-function pow(x, n){
-    return x**n; //возвращается x в степени n
-} 
+function pow(x, n) {
+    return x ** n; //возвращается x в степени n
+}
+
+/**
+ * Возвращает сумму чисел от 1 до n включительно.
+ * @param {number} n Число, до которого вычисляется сумма, должно быть натуральным числом.
+ * @return {number} sum, сумму чисел от 1 до n включительно.
+ */
 
 //функция суммы чисел от 1 до n
-function sumTo(n){
-    return (n*(n+1))/2 //формула суммы чисел
+function sumTo(n) {
+    return (n * (n + 1)) / 2 //формула суммы чисел
 
 }
+
+/**
+ * Возвращает факториал числа n!.
+ * @param {number} n Число, факториал которого вычисляется, должно быть натуральным числом.
+ * @return {number} res, факториал числа n!.
+ */
 
 //функция факториала числа n
-function factorial(n){
-    if (n<=0)
-    {
+function factorial(n) {
+    if (n <= 0) {
         return 1n;
     }
-    else{
-        return ( BigInt (n) *  BigInt(factorial(n-1))); //возвращает произведение числа n и факториала n-1
+    else {
+        return (BigInt(n) * BigInt(factorial(n - 1))); //возвращает произведение числа n и факториала n-1
     }
 }
 
-//n-ое число фибоначи
-function fib(n){
-    a = BigInt(1);
-    b=BigInt(1);
-    if (n== BigInt(0))
-    {
-        return BigInt(0);
+/**
+ * Возвращает n-е число Фибоначчи.
+ * @param {number} n Порядковый номер числа Фибоначчи, должен быть натуральным числом.
+ * @return {number} b, n-е число Фибоначчи.
+ */
+
+function fib(n) {
+    let a = 1n;
+    let b = 1n;
+    if (n == 0n) {
+        return 0n;
     }
-    else
-    {
-        for (i = BigInt(3); i<=n; i++)
-        {
-            c = a+b; //цикл для числа как суммы двух предыдущих его чисел
-            a=b;
-            b=c;
+    else {
+        for (let i = 3n; i <= n; i++) {
+            let c = a + b;
+            a = b;
+            b = c;
         }
         return b;
     }
 }
 
+/**
+ * Возвращает результат сравнения значений y и x.
+ * @param {number} x Первый аргумент, должен быть целочисленным.
+ * @param {number} y Второй аргумент, должен быть целочисленным.
+ * @return {function} Анонимная функция, которая вернет true, false или null
+    
+ }} Результат сравнения.
+ */
+
 //сравнение числа x и числа y из анонимной функции
-function compare(x){
-return function(y){ //сама анониная функция
-    if (y>x) return true;
-    else if (y<x) return false;
-    else if (y==x) return null;
+function compare(x) {
+    return function (y) { //сама анониная функция
+        if (y > x) return true;
+        else if (y < x) return false;
+        else if (y == x) return null;
+    }
 }
-}
+
+/**
+ * Возвращает сумму всех своих аргументов.
+ * @param {number} Значения аргументов.
+ * @return {number} result, сумма всех своих аргументов.
+ */
+
+
 //функция суммы всех своих аргументов
-function sum(){
+function sum() {
     var result = 0;
-    for (var i = 0; i < arguments.length; i++)
-    {
-        result+=arguments[i]
+    for (var i = 0; i < arguments.length; i++) {
+        result += arguments[i]
     }
     return result;
 }
--- js3/lab3.js
+++ js3/lab3.js
@@ -1,5 +1,12 @@
 "use strict";
-import {fib} from './lab2.js';
+import { fib } from './lab2.js';
+
+/**
+ * Возвращает дробную часть числа num.
+ *
+ * @param {number} num число, от которого нужно найти дробную часть.
+ * @return {number} дробная часть числа.
+ */
 
 //функция возврата дробной части числа
 export function getDecimal(num) {
@@ -13,67 +20,113 @@ export function getDecimal(num) {
         return +(num - Math.trunc(num)).toFixed(2);
     }
 }
-       
-    
 
+
+/**
+ * Возвращает строку str с заглавным первым символом.
+ *
+ * @param {string} str строка, которую нужно преобразовать.
+ * @return {string} str с заглавным первым символом.
+ */
 
 
 //функция возврата строки с первым заглавным символом
-export function ucFirst(str){
+export function ucFirst(str) {
     if (!str) return str; //Проверка на пустую строку
     return str[0].toUpperCase() + str.slice(1); // создаём новую строку на основе предыдущей
-}  
+}
 
+/**
+ * Возвращает true, если строка str содержит 'viagra' или 'XXX', а иначе false.
+ *
+ * @param {string} str проверяемая строка.
+ * @return {boolean} true, если строка str содержит 'viagra' или 'XXX', а иначе false.
+ */
 
 //функция проверки строки
-export  function checkSpam(str){
+export function checkSpam(str) {
     let lowerStr = str.toLowerCase(); // строка переводится в нижний регистр
-    return  lowerStr.includes("xxx") || lowerStr.includes("viagra");
+    return lowerStr.includes("xxx") || lowerStr.includes("viagra");
 }
+
+/**
+ * Проверяет длину строки str.
+ * если она превосходит maxlength – заменяет 
+ * конец str на символ многоточие "…"
+ * 
+ * @param {string} str Проверяемая строка.
+ * @param {number} maxlength максимальная длина строки.
+ * @return {string} str, усеченная строка.
+ */
 
 //функция усечения строки
-export  function truncate(str, maxlength){
+export function truncate(str, maxlength) {
     return (str.length > maxlength) ? //Если строка превосходит maxlength -
-    str.slice(0, maxlength - 1) + '…' : str; // убираем на один символ больше при обрезании строки
+        str.slice(0, maxlength - 1) + '…' : str; // убираем на один символ больше при обрезании строки
 }
+
+/**
+ * Дефисы в строке удаляются, а все слова 
+ * после них получают заглавную букву.
+ * 
+ * @param {string} str Проверяемая строка.
+ * @return {string} StrNew, преобразованная строка.
+ */
 
 //функция преобразования строки
-export  function camelize(str){
+export function camelize(str) {
     let strArr = str.split("-");
     let strNew = "";
-     for (let i = 0; i <= strArr.length - 1; i++){
-      if (i == 0) {
-         strNew = strArr[0];
-      } else strNew += ucFirst(strArr[i]);
-      
-      } return strNew;
+    for (let i = 0; i <= strArr.length - 1; i++) {
+        if (i == 0) {
+            strNew = strArr[0];
+        } else strNew += ucFirst(strArr[i]);
+
+    } return strNew;
 
 }
 
-// функция массива чисел фибоначи
-export function fibs(n){
+/**
+ * Возвращает массив, заполненный числами 
+ * Фибоначчи до n-го числа (не включая его).
+ * 
+ * @param {number} n натуральноее число, количество чисел Фибоначчи.
+ * @return {Array} arr, массив с числами Фибоначчи.
+ */
+export function fibs(n) {
     let arr = [];
-    for(let i = 0; i < n; i++){
-        arr.push(fib(i)); // для каждой итерации добавляет в массив i-е число фибоначи вплоть до n
-    }
+    for (let i = 0; i < n; i++) arr.push(fib(i));
     return arr;
 }
 
+/**
+ * Возвращает массив из элементов, отсортированный по убыванию.
+ *
+ * @param {Array} arr массив элементов.
+ * @return {Array} arr, массив, отсортированный по убыванию.
+ */
 
 export function arrReverseSorted(arr) {
     return arr.slice().sort(function (a, b) { //slice() создает копию исходного массива, метод sort() сортирует новый массив в порядке убывания с помощью функции сравнения
-      return b - a;//принимает два аргумента a и b и возвращает результат вычисления b - a. Если результат вычисления отрицательный, то элемент a будет располагаться перед элементом b в массиве
+        return b - a;//принимает два аргумента a и b и возвращает результат вычисления b - a. Если результат вычисления отрицательный, то элемент a будет располагаться перед элементом b в массиве
     });
-  }
+}
+
+/**
+* Возвращает массив уникальных, не повторяющихся значений.
+*
+* @param {Array} arr массив элементов.
+* @return {Array} arr, массив с уникальными значениями.
+*/
 
 export function unique(arr) {
     let result = [];
-  
+
     for (let str of arr) { //проверяем массив с результатом. Если элемент из arr есть в результате - игнорируем его
-      if (!result.includes(str)) { // иначе - включаем в массив
-        result.push(str);
-      }
+        if (!result.includes(str)) { // иначе - включаем в массив
+            result.push(str);
+        }
     }
-  
+
     return result;
-  }
+}
